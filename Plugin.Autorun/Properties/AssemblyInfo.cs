3f219b5f71102948e36773fdd1ba89c56431e1b6
DKorablin2 <danila.korablin@unity3d.com>
2025-02-23 23:59:50 +0300

    Unified plugins deployment

--- Plugin.Autorun/Properties/AssemblyInfo.cs
+++ Plugin.Autorun/Properties/AssemblyInfo.cs
@@ -1,7 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: ComVisible(false)]
 [assembly: Guid("1c4a8e73-b448-4e35-ad86-9ab8abc342c1")]
 [assembly: System.CLSCompliant(true)]
 
@@ -9,14 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyMetadata("ProjectUrl", "https://dkorablin.ru/project/Default.aspx?File=85")]
 #else
 
-[assembly: AssemblyTitle("Plugin.Autorun")]
 [assembly: AssemblyDescription("Autostart application on system startup")]
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
-[assembly: AssemblyCompany("Danila Korablin")]
-[assembly: AssemblyProduct("Plugin.Autorun")]
 [assembly: AssemblyCopyright("Copyright © Danila Korablin 2011-2012")]
 #endif
